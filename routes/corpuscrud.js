6249102ee38ad664164180dddcd8ac4b3b9a908c
Jakub Kuklok <kuklok.jakub@fh-swf.de>
2020-02-11 13:38:05 +0100

    TF_01: Made CRUD functionality "generic" to reduce copy pasta.

--- routes/corpuscrud.js
+++ routes/corpuscrud.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let corpusController = require('../controller/corpuscontroller');
+let crud = require('./crud');
 
 let router = express.Router({mergeParams: true});
 
@@ -11,62 +12,14 @@ let router = express.Router({mergeParams: true});
  * DELETE /CRUD/corpus/{c_id}  deleteOne
  */
 
-router.get('/', (req, res, next) => {
-    corpusController.listAll().then((items)=> {
-        res.send(items);
-    }).catch((err)=>{
-        res.status(500).send(err);
-    });
-});
+router.get('/', crud.listAll(corpusController));
 
-router.post('/', (req, res, next) => {
-    console.info("creating new corpus:");
-    corpusController.createOne(req.body).then((newItem)=> {
-        res.status(200).send(newItem);
-    }).catch((err)=>{
-        console.error(err);
-        res.status(500).send(err);
-    });
-});
+router.post('/', crud.createOne(corpusController));
 
-router.get('/:c_id', (req, res, next) => {
-    console.info("get corpus with id: " + req.params.c_id);
-    corpusController.getOne(req.params.c_id).then((item)=> {
-        if (item == null) {
-            res.sendStatus(404);
-        } else {
-            res.status(200).send(item);
-        }
-    }).catch((err)=>{
-        res.status(500).send(err);
-    });
-});
+router.get('/:c_id', crud.getOne(corpusController, 'c_id'));
 
-router.put('/:c_id', (req, res, next) => {
-    console.info("updating corpus with id: " + req.params.c_id);
-    corpusController.updateOne(req.params.c_id, req.body).then((updatedItem)=> {
-        if (updatedItem) {
-            res.status(200).send(updatedItem);
-        } else {
-            res.sendStatus(404);
-        }
-    }).catch((err)=>{
-        res.status(500).send(err);
-    });
-});
+router.put('/:c_id', crud.updateOne(corpusController, 'c_id'));
 
-
-router.delete('/:c_id', (req, res, next) => {
-    console.info("deleting corpus with id: " + req.params.c_id);
-    corpusController.deleteOne(req.params.c_id).then((deleted)=> {
-        if (deleted) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(500); // TODO better responses....
-        }
-    }).catch((err)=>{
-        res.status(500).send(err);
-    });
-});
+router.delete('/:c_id', crud.deleteOne(corpusController, 'c_id'));
 
 module.exports = router;
