b70138707dd71492ed5a90098a21f95518b70642
Timo Neuhaus <timo@n.euhaus.net>
2020-02-16 20:09:00 +0100

    For some reason commit c82f727969a30f5ef5394782d587a3b17b7f22aa removed a test route which is required for checking if service is available

--- routes/IndexController.js
+++ routes/IndexController.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('Welcome to TagFlip!');
 });
 
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+  res.status(200).send({status: "available"});
+});
+
 module.exports = router;
